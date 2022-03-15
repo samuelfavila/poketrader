bb5f754f967484e1cbf31a4442bdec111bc52d18
Samuel Avila <samuel2001.furtadoavila@gmail.com>
2022-03-15 16:38:17 -0300

    Add Header Component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Header = () => {
+	return (
+		<div className="container-header">
+			<h1>Poketrader</h1>
+		</div>
+	);
+};
+
+export default Header;
